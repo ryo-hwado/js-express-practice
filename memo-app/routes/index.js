7471b52c3e0c50a0c4d6084357ad32d7f6748022
h.ryo <h.ryo@ivisystem.com>
2021-07-03 01:29:59 +0900

    setup todo-app and sqlite3

--- memo-app/routes/index.js
+++ memo-app/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+let todos = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'TODOリスト', 
+    todos: todos});
 });
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  const todo = req.body.add;
+  console.log(todo);
+  todos.push(todo);
+
+  res.redirect('/');
+});
+
 
 module.exports = router;
