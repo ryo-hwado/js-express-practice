d12252ace3e9de5f35926518d5c5c1780a265856
h.ryo <h.ryo@ivisystem.com>
2021-07-05 01:59:11 +0900

    TODO App Complete

--- todo-app/routes/index.js
+++ todo-app/routes/index.js
@@ -3,14 +3,15 @@ const router = express.Router();
 
 const sqlite3 = require('sqlite3');
 
-//データベースオブジェクトの取得
+// データベースオブジェクトの取得
 const db = new sqlite3.Database('db/todo-list-data.db');
 
 let todos = [];
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  
+// ルートディレクトリ配下のルーティング
+// GET
+router
+.get('/', function(req, res, next) {  
   db.serialize(() => {
     db.all("SELECT * FROM todos", (err, rows) => {
       if(!err) {
@@ -21,13 +22,45 @@ router.get('/', function(req, res, next) {
       }
     });
   });
-
+})
+.post('/', function(req, res, next) {
+  const todo = req.body.add;
+  db.run('INSERT INTO todos (content) values (?)', todo, (err) => {
+    if (!err) {
+      res.redirect('/');
+    }
+  });
 });
 
-router.post('/', function(req, res, next) {
-  const todo = req.body.add;
-  db.run('INSERT INTO todos (content) values (?)', todo);
-  res.redirect('/');
+// /:id 以下のルーティングを指定
+router
+.get('/:id', function(req, res, next) {
+  db.all("SELECT * FROM todos WHERE id = ?", req.params.id, (err, rows) => {
+      if(!err) {
+          res.render('edit', {
+              title: "ID: " + req.params.id,
+              todo: rows[0]
+          });
+      }
+  });
+})
+.post('/:id', function(req, res, next) {
+  db.run("UPDATE todos SET content = ? WHERE id = ?", req.body.modify ,req.params.id, (err, rows) => {
+      if(!err) {
+          res.redirect('/');
+      }
+  });
+})
+.get('/:id/delete', function(req, res, next) {
+  db.run("DELETE FROM todos WHERE id = ?", req.params.id, (err, rows) => {
+      if(!err) {
+          res.redirect('/');
+      }
+  });
+})
+
+router.get('/favicon.ico', (req, res) => {
+  res.sendFile("/public/favicon.ico");
 });
 
 module.exports = router;
